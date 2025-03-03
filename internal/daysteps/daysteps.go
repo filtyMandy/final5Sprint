528ff17f958c8f823f481385360d8e64a5707a3c
Pavlinov Anton <reastandpeas@gmail.com>
2025-03-03 14:57:09 +0300

    working build 1

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,7 +1,12 @@
 package daysteps
 
 import (
-	...
+	"fmt"
+	"github.com/Yandex-Practicum/go1fl-sprint5-final/internal/personaldata"
+	"github.com/Yandex-Practicum/go1fl-sprint5-final/internal/spentenergy"
+	"strconv"
+	"strings"
+	"time"
 )
 
 const (
@@ -9,11 +14,30 @@ const (
 )
 
 // создайте структуру DaySteps
-...
-
+type DaySteps struct {
+	Steps    int
+	Duration time.Duration
+	personaldata.Personal
+}
 
 // создайте метод Parse()
-...
+func (ds *DaySteps) Parse(datastring string) (err error) {
+	parts := strings.Split(datastring, ",")
+	if len(parts) != 2 {
+		return fmt.Errorf("conversion error: %w\ndata: %s", err, datastring)
+	}
+
+	ds.Steps, err = strconv.Atoi(parts[0])
+	ds.Duration, err = time.ParseDuration(parts[1])
+	return
+}
 
 // создайте метод ActionInfo()
-...
+func (ds DaySteps) ActionInfo() string {
+	distance := spentenergy.Distance(ds.Steps)
+	kal := spentenergy.WalkingSpentCalories(ds.Steps, ds.Weight, ds.Height, ds.Duration)
+
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал. \n",
+		ds.Steps, distance, kal,
+	)
+}
--- internal/trainings/trainings.go
+++ internal/trainings/trainings.go
@@ -1,16 +1,54 @@
 package trainings
 
 import (
-	...
+	"fmt"
+	"github.com/Yandex-Practicum/go1fl-sprint5-final/internal/personaldata"
+	"github.com/Yandex-Practicum/go1fl-sprint5-final/internal/spentenergy"
+	"strconv"
+	"strings"
+	"time"
 )
 
 // создайте структуру Training
-...
-
+type Training struct {
+	Steps        int
+	TrainingType string
+	Duration     time.Duration
+	personaldata.Personal
+}
 
 // создайте метод Parse()
-...
+func (t *Training) Parse(datastring string) (err error) {
+	parts := strings.Split(datastring, ",")
+	if len(parts) != 3 {
+		return fmt.Errorf("conversion error: %w\ndata: %s", err, datastring)
+	}
+
+	t.Steps, err = strconv.Atoi(parts[0])
+
+	t.TrainingType = parts[1]
+
+	t.Duration, err = time.ParseDuration(parts[2])
+
+	return
+}
 
 // создайте метод ActionInfo()
-...
+func (t Training) ActionInfo() string {
+	distance := spentenergy.Distance(t.Steps)
+	speed := spentenergy.MeanSpeed(t.Steps, t.Duration)
 
+	var kal float64
+	switch t.TrainingType {
+	case "Ходьба":
+		kal = spentenergy.WalkingSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
+	case "Бег":
+		kal = spentenergy.RunningSpentCalories(t.Steps, t.Weight, t.Duration)
+	default:
+		return fmt.Sprintf("Неизвестный тип тренировки: %s", t.TrainingType)
+	}
+
+	return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n",
+		t.TrainingType, t.Duration.Hours(), distance, speed, kal,
+	)
+}
